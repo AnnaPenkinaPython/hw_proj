ebdb8fbc59047f9c07e138a45d9f467c33c84d08
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-02-01 22:53:04 +0700

    function added again

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def upper_word(word):
     word = word.upper()
     return word
+
+def upper_each_word(word):
+    """function converts each word's first letters to up register"""
+    word = str(word.title())
+    return word
